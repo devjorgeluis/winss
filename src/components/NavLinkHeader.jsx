5143630f23b5aa50faa65ca22f11d46e094ba64d
devjorgeluis <jorgencnc0608@gmail.com>
2025-10-10 13:57:05 -0400

    fix navbar active color

--- src/components/NavLinkHeader.jsx
+++ src/components/NavLinkHeader.jsx
@@ -6,7 +6,7 @@ const NavLinkHeader = (props) => {
             <NavLink
                 to={props.pageCode === "home" ? "/" : `/${props.pageCode}`}
                 className={({ isActive }) =>
-                    `btn-menu-cj button type1 text-white ${isActive ? "active-menu-item" : ""}`
+                    `btn-menu-cj button type1 ${isActive ? "active-menu-item" : ""}`
                 }
                 onClick={props.onClick}
             >
